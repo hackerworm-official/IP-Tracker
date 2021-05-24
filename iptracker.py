6eb1196e139004c45f7697cd15189e5a4f1342ea
ʜᴀᴄᴋᴇʀ ᴡᴏʀᴍ <84688262+hackerworm-official@users.noreply.github.com>
2021-05-24 05:31:34 +0530

    Add files via upload

---
+++ iptracker.py
@@ -0,0 +1,42 @@
+import urllib.request as ur
+import json
+import webbrowser
+
+print(f"""
+
+ /$$$$$$ /$$$$$$$        /$$$$$$$$                           /$$                          
+|_  $$_/| $$__  $$      |__  $$__/                          | $$                          
+  | $$  | $$  \ $$         | $$  /$$$$$$  /$$$$$$   /$$$$$$$| $$   /$$  /$$$$$$   /$$$$$$ 
+  | $$  | $$$$$$$/         | $$ /$$__  $$|____  $$ /$$_____/| $$  /$$/ /$$__  $$ /$$__  $$
+  | $$  | $$____/          | $$| $$  \__/ /$$$$$$$| $$      | $$$$$$/ | $$$$$$$$| $$  \__/
+  | $$  | $$               | $$| $$      /$$__  $$| $$      | $$_  $$ | $$_____/| $$      
+ /$$$$$$| $$               | $$| $$     |  $$$$$$$|  $$$$$$$| $$ \  $$|  $$$$$$$| $$      
+|______/|__/               |__/|__/      \_______/ \_______/|__/  \__/ \_______/|__/      
+                                                                                                                           
+                                Telegram Channel: @hackingworm                                              
+                                                                                            """)
+
+
+webbrowser.open("https://t.me/hackingworm")
+
+while True:
+    ip=input("Which IP would you like to check sir ? : ")
+    url= "http://ip-api.com/json/"
+    response = ur.urlopen(url + ip)
+    data = response.read()
+    Values = json.loads(data)
+
+    print(" IP: " + Values["query"])
+    print(" Status: " + Values["status"])
+    print(" country: " + Values["country"])
+    print(" counterycode: " + Values["countryCode"])
+    print(" Region: " + Values["region"])
+    print(" Region name: " + Values["regionName"])
+    print(" City: " + Values["city"])
+    print(" ZIP: " + Values["zip"])
+    print(" ISP: " + Values["isp"])
+    print(" Time zone: " + Values["timezone"])
+    print(" Longitude: " + str(Values["lon"]))
+    print(" Lattitude: " + str(Values["lat"]))
+
+    break
